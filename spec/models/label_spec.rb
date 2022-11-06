5aa4052e109920085f81b5dd1a06b019ab0808d4
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-11-07 03:23:41 +0900

    label modelspec実装

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe 'ラベルモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'ラベルの名前が空文字の場合' do
       it 'バリデーションに失敗する' do
+        label = Label.create(name: '')
+        expect(label).not_to be_valid
       end
     end
 
     context 'ラベルの名前に値があった場合' do
       it 'バリデーションに成功する' do
+        label = Label.create(name: 'label1')
+        expect(label).to be_valid
       end
     end
   end
